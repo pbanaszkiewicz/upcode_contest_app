634dba14dbe555a64660d7eabedb820b6ec1ec32
Piotr Banaszkiewicz <pbnan@pbnan.pl>
2011-03-09 12:09:05 +0100

    Polish translation and new exe file

---
+++ stecrypt_pl.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="15"/>
+        <source>Stecrypt</source>
+        <translation>Stecrypt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>Select action</source>
+        <translation>Wybierz działanie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="34"/>
+        <source>Encryption</source>
+        <translation>Szyfrowanie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="44"/>
+        <source>Decryption</source>
+        <translation>Deszyfrowanie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <source>Use webcam to select input key or instead type in some huge number into the Number field below.</source>
+        <translation>Użyj kamery internetowej by wybrać klucz albo wpisz w pole Numer poniżej jakąś dużą liczbę.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <source>Webcam</source>
+        <translation>Kamera internetowa</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <location filename="mainwindow.cpp" line="40"/>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>Launch</source>
+        <translation>Uruchom</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <source>Finished!</source>
+        <translation>Koniec!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>Number</source>
+        <translation>Numer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>Image</source>
+        <translation>Obraz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="143"/>
+        <source>Load</source>
+        <translation>Ładuj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="153"/>
+        <source>Save</source>
+        <translation>Zapisz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="179"/>
+        <source>Text</source>
+        <translation>Tekst</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <location filename="mainwindow.cpp" line="87"/>
+        <source>Encrypt</source>
+        <translation>Szyfruj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="35"/>
+        <source>Stop</source>
+        <translation>Stop</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
+        <source>Uninitialized</source>
+        <translation>Niezainicjalizowany</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="65"/>
+        <source>Initialized</source>
+        <translation>Zainicjalizowany</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="68"/>
+        <source>Select object to track</source>
+        <translation>Wybierz obiekt do śledzenia</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="71"/>
+        <source>Tracking</source>
+        <translation>Śledzenie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>Finished</source>
+        <translation>Zakończono</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="79"/>
+        <source>Interrupted</source>
+        <translation>Przerwano</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Decrypt</source>
+        <translation>Deszyfruj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="100"/>
+        <source>Open image</source>
+        <translation>Otwórz obraz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="101"/>
+        <location filename="mainwindow.cpp" line="128"/>
+        <source>Bitmaps (*.bmp)</source>
+        <translation>Bitmapy (*.bmp)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="114"/>
+        <source>Loaded %1</source>
+        <translation>Załadowano %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="119"/>
+        <source>Cannot load %1 - too little depth</source>
+        <translation>Nie można załadować %1 - zbyt mała głębia koloru</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="127"/>
+        <source>Save image</source>
+        <translation>Zapisz obraz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="132"/>
+        <source>Saved %1</source>
+        <translation>Zapisano %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="134"/>
+        <source>Cannot save %1</source>
+        <translation>Nie można zapisać %1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="159"/>
+        <source>Encryption done</source>
+        <translation>Szyfrowanie skończone</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="164"/>
+        <source>Decryption done</source>
+        <translation>Deszyfrowanie skończone</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="175"/>
+        <source>Input image has bad format</source>
+        <translation>Obraz wejściowy ma zły format</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="179"/>
+        <source>Input text is too long</source>
+        <translation>Tekst wejściowy jest za długi</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="183"/>
+        <source>Input image is too small. Should be at least 46*46px</source>
+        <translation>Obraz wejściowy jest za mały. Powinien być przynajmniej 46*46px</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="187"/>
+        <source>Input number is not set</source>
+        <translation>Liczba wejściowa nie jest ustawiona</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="196"/>
+        <source>Too much characters! The text will be stripped to 512 chars</source>
+        <translation>Za dużo znaków! Tekst zostanie obcięty do 512 znaków</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="198"/>
+        <source>The text contains disallowed characters!</source>
+        <translation>Tekst zawiera niedozwolone znaki!</translation>
+    </message>
+</context>
+</TS>
